4f3d50e50498efcc05197ed3a569b82490d9dd0f
Joshua Barratt <jbarratt@serialized.net>
2017-06-06 00:22:40 -0700

    updated readme and deploy script

---
+++ tools/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+export AWS_PROFILE="serialized"
+cd ~/work/serialized-hugo
+rm -rf public
+hugo
+s3deploy -bucket serialized -region "us-west-2" -source public/
+
+# aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id E374LZZY9EPZLY --paths /index.html
