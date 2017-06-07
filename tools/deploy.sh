935ccbc4627583d8c06340615ea13a719549e0f2
Joshua Barratt <jbarratt@serialized.net>
2017-06-07 09:03:58 -0700

    added cli way not to issue invalidation

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -6,7 +6,9 @@ export AWS_PROFILE="serialized"
 cd ~/work/serialized-hugo
 rm -rf public
 hugo
-s3deploy -bucket serialized -region "us-west-2" -source public/
+s3deploy -bucket serialized -region "us-west-2" -workers 4 -source public/
 
 # aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id E374LZZY9EPZLY --paths '/*'
+if [[ "$1" != "noinv" ]]; then
+       aws cloudfront create-invalidation --distribution-id E374LZZY9EPZLY --paths '/*'
+fi
